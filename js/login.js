f72138124efd8baa11de69272a7bb682d2217429
QAQSQY <44799618+QAQSQY@users.noreply.github.com>
2019-11-03 17:17:09 +0800

    添加了主页面和登陆页面

---
+++ js/login.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	$('#loginSubmit').click(function(event) {
+		var account = $('#account').val();
+		var password = $('#password').val();
+		if (account == '' && password == '') {
+			event.preventDefault();
+			event.stopPropagation();
+			return $.growl.warning({
+				title: "警告",
+				message: "密码或银行卡号不能为空"
+			});
+		} else {
+			$('form').submit(function() {
+				console.log(this);
+			});
+		}
+
+	});
+});
+
+//阻止表单内容为空时默认跳转
+function preventSubmit() {
+	var account = $('#account').val();
+	var password = $('#password').val();
+	if (account == '' && password == '') {
+
+		return false;
+	} else {
+		$('form').submit(function() {
+			console.log(this);
+		});
+	}
+}
